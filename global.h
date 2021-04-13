80051a7cada64e0a1802acc7fc76628de548013c
catherinehealyy <58479939+catherinehealyy@users.noreply.github.com>
2021-04-13 13:54:35 -0400

    Update global.h

--- global.h
+++ global.h
@@ -1,26 +1,31 @@
-#include "stdbool.h"
-#include <limits.h>
-
-struct evTable{
-	char var[128][100];
-	char word[128][100];
+// This is ONLY a demo micro-shell whose purpose is to illustrate the need for and how to handle nested alias substitutions and Flex start conditions.
+// This is to help students learn these specific capabilities, the code is by far not a complete nutshell by any means.
+struct evTable {
+   char var[128][100];
+   char word[128][100];
 };
 struct aTable {
 	char name[128][100];
 	char word[128][100];
 };
-struct comTable {
-	char com[128][100];
+struct bcTable {
+	char command[128][100];
+	int argCount[128];
+	char comArgs[128][100];
+	char temp[128][100];
 	char input[128][100];
 	char output[128][100];
 };
 
-char cwd[PATH_MAX];
+//char cwd[_MAX_PATH];
+
 
 struct evTable varTable;
-
 struct aTable aliasTable;
+struct bcTable commandTable;
 
-int aliasIndex, varIndex;
+int aliasIndex, varIndex, bcIndex;
+
+char* args(char* name);
 
 char* subAliases(char* name);
